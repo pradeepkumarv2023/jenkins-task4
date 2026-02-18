ad214e0ba1725eddfffbcf7e7fabca977c2af968
pradeepkumar.v <pradeepkumar.v2023@vitstudent.ac.in>
2026-02-18 16:37:33 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkinsfile Pipeline!");
+        System.out.println("CI/CD Pipeline executed successfully!");
     }
 }
